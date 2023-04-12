eb31619b5665605f1a4c8742646af243b814e592
Raj Singh <rsingh40@chicagobooth.edu>
2023-04-12 23:06:09 +0000

    second

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,39 @@
 class ApplicationController < ActionController::Base
 
   def blank_square_form
-
     render({:template => "calculation_templates/square_form.html.erb"})
   end
+
   def blank_square_root_form
 
     render({:template => "calculation_templates/square_root_form.html.erb"})
   end
+
   def blank_payment_form
 
     render({:template => "calculation_templates/payment_form.html.erb"})
   end
+
   def blank_random_number_form
 
     render({:template => "calculation_templates/random_number_form.html.erb"})
   end
 
+  def calculate_square
+      render({:template => "calculation_templates/square_results.html.erb"})
+  end
 
+  def calculate_square_root
+      render({:template => "calculation_templates/square_root_results.html.erb"})
+  end
 
+  def calculate_payment
+    render({:template => "calculation_templates/payment_results.html.erb"})
+  end
+
+  def calculate_random_number
+    render({:template => "calculation_templates/random_number_results.html.erb"})
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
 
   get("/square/new", { :controller => "application", :action => "blank_square_form" })
   get("/square_root/new", { :controller => "application", :action => "blank_square_root_form" })
-  get("/payment/new", { :controller => "application", :action => "payment" })
+  get("/payment/new", { :controller => "application", :action => "blank_payment_form" })
   get("/random/new", { :controller => "application", :action => "blank_random_number_form" })
+
+  get("/square/results", { :controller => "application", :action => "calculate_square" })
+  get("/square_root/results", { :controller => "application", :action => "calculate_square_root" })
+  get("/payment/results", { :controller => "application", :action => "calculate_payment" })
+  get("/random/results", { :controller => "application", :action => "calculate_random_number" })
 
 
 end
