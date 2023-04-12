9c1beaceffbc26ec72018ef9d7751c9848b107c2
Raj Singh <rsingh40@chicagobooth.edu>
2023-04-12 22:39:50 +0000

    first

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
 
+  def blank_square_form
+
+    render({:template => "calculation_templates/square_form.html.erb"})
+  end
+  def blank_square_root_form
+
+    render({:template => "calculation_templates/square_root_form.html.erb"})
+  end
+  def blank_payment_form
+
+    render({:template => "calculation_templates/payment_form.html.erb"})
+  end
+  def blank_random_number_form
+
+    render({:template => "calculation_templates/random_number_form.html.erb"})
+  end
+
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
 
+  get("/square/new", { :controller => "application", :action => "blank_square_form" })
+  get("/square_root/new", { :controller => "application", :action => "blank_square_root_form" })
+  get("/payment/new", { :controller => "application", :action => "payment" })
+  get("/random/new", { :controller => "application", :action => "blank_random_number_form" })
+
+
 end
